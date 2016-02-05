e351025dd85be48de4d8688d9eee8e3d1e31814d
Omar Skalli <oskalli@andrew.cmu.edu>
2016-02-04 23:21:47 -0500

    Initial Commit

---
+++ socialBeacon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  socialBeacon
+//
+//  Created by Omar Skalli on 2/4/16.
+//  Copyright © 2016 DreamCode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
