a7028ee103f84fd58e46550386cbf680a1daa509
arjunh <arjunh@andrew.cmu.edu>
2016-02-05 20:58:13 -0500

    Integrate Parse into project

--- socialBeacon/AppDelegate.swift
+++ socialBeacon/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+            Parse.enableLocalDatastore()
+            Parse.setApplicationId("vMbH2IEMsBppsj3AuCE6IphvZzwa8L3xxxNwUwGe", clientKey: "lW2fhJ5VlNEOtdWTjvICkp2WwP0HyX6hHAk2jOyK")
         // Override point for customization after application launch.
         return true
     }
