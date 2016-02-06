aadc676b716b175b31f8e7256c0b91b1b60860c0
Roy S. Shin <seungwos@bh140d-b4.bh.andrew.cmu.edu>
2016-02-06 03:23:25 -0500

    version 1

--- socialBeacon/ViewController.swift
+++ socialBeacon/ViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate, AVAudioRecorderDelegate {
 
+    @IBOutlet var RecordBTN: UIButton!
+    @IBOutlet var PlayBTN: UIButton!
+    @IBOutlet var filelabel: UIButton!
+
+    
+    var soundRecorder: AVAudioRecorder!
+    var recordingSession: AVAudioSession!
+    
+    var fileName = "audioFile.m4a"
+    
+    let recordSettings =
+    [
+        AVFormatIDKey : Int(kAudioFormatMPEGLayer3),
+        AVEncoderBitRateKey: 320000 as NSNumber,
+        AVNumberOfChannelsKey: 1 as NSNumber,
+        AVEncoderAudioQualityKey: AVAudioQuality.High.rawValue as NSNumber,
+        AVSampleRateKey: 44100.0 as NSNumber,
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        PlayBTN.enabled = false
         // Do any additional setup after loading the view, typically from a nib.
+        setupRecorder()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +43,79 @@ class ViewController: UIViewController {
     }
 
 
-}
+    
+    func setupRecorder(){
+        recordingSession = AVAudioSession.sharedInstance()
+    
+        do {
+            try recordingSession.setCategory(AVAudioSessionCategoryPlayAndRecord)
+            try recordingSession.setActive(true)
+            recordingSession.requestRecordPermission() { [unowned self] (allowed: Bool) -> Void in
+                dispatch_async(dispatch_get_main_queue()) {
+                    if allowed {
+                        self.loadRecordingUI()
+                    } else {
+                        self.loadFailUI()
+                    }
+                }
+            }
+        } catch {
+            self.loadFailUI()
+        }
+    }
 
+    func loadRecordingUI() {
+    
+    }
+
+    func loadFailUI() {
+    
+    }
+    
+    @IBAction func Record(sender: UIButton) {
+        
+        if sender.titleLabel?.text == "Record"{
+            
+            startRecording("Downloads", duration: 10.0)
+            PlayBTN.enabled = false
+            sender.setTitle("Stop", forState: .Normal)
+            
+        } else {
+            if soundRecorder?.recording == true {
+                soundRecorder?.stop()
+            }
+            sender.setTitle("Record", forState: .Normal)
+            
+        }
+    }
+    
+    func getDocumentsDirectory() -> NSString {
+        let paths = NSSearchPathForDirectoriesInDomains(.DownloadsDirectory, .UserDomainMask, true) as [String]
+        let documentsDirectory = paths[0]
+        
+        print("documentsDirectory")
+        return documentsDirectory
+    }
+    
+    func getClipURL(recordingName : String) -> NSURL {
+        let audioFilename = getDocumentsDirectory().stringByAppendingPathComponent(recordingName + ".m4a")
+        let audioURL = NSURL(fileURLWithPath: audioFilename)
+        return audioURL
+    }
+    
+    func startRecording(saveAudioDirectory: String?, duration: NSTimeInterval) {
+        let audioURL: NSURL = recordAudio.getClipURL("recording")
+        filelabel.setTitle(audioURL.absoluteString, forState: .Normal)
+
+        do {
+            soundRecorder = try AVAudioRecorder(URL: audioURL, settings: self.recordSettings)
+            soundRecorder.delegate = self
+            soundRecorder.record()
+        } catch {
+
+        }
+    }
+    func audioRecorderDidFinishRecording(recorder: AVAudioRecorder, successfully flag: Bool) {
+        //hello
+    }
+}
